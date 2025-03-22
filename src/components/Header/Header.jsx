27dc3a21e6e7952385ce7e4e1200d8292b111ee4
falmeidaco <felipealmeidacosta@gmail.com>
2025-03-22 15:31:43 -0300

    adicionando font inter no lugar de helvetica

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,39 @@ import monkLogo from '../../assets/monks.svg'
 import styled from "styled-components";
 
 export const SHeader = styled.header`
+background-color: #2D2D2D;
+  height: var(--header-height);
+  padding: 0 3rem;
   display: flex;
-  background-color: #2D2D2D;
+  align-items: center;
   color: #EAE8E4;
+  gap: 2rem;
+  nav ul {
+    display: flex;
+    gap: 1rem;
+    list-style: none;
+    margin:0;
+    padding:0;
+    a {
+      font-weight: 200;
+      color:white;
+      text-decoration:none;
+      display:flex;
+      align-items:center;
+      gap:.5rem;
+      &:before {
+        content: "•";
+        visibility:hidden;
+        font-size: 2rem;
+      }
+      &:hover {
+        font-weight: 400;
+        &:before {
+          visibility:visible;
+        }
+      }
+    }
+  }
 `;
 
 export default function Header() {
@@ -16,7 +46,16 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <a href="#">Link 1</a>
+            <a href="#">Categoria 1</a>
+          </li>
+          <li>
+            <a href="#">Categoria 2</a>
+          </li>
+          <li>
+            <a href="#">Categoria 3</a>
+          </li>
+          <li>
+            <a href="#">Categoria 4</a>
           </li>
         </ul>
       </nav>
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,15 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+
+  :root {
+    --header-height: 80px;
+  }
+
   html, body {
     min-height: 100vh;
     margin: 0;
     padding: 0;
     background-color: #EAE8E4;
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
   }
   #root {
     margin: 0 auto;
-    max-width: 1200px;
   }
 `;
