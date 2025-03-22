5fb68e4c3cb4ad7ee94b6df15fee3ffd03430f56
falmeidaco <felipealmeidacosta@gmail.com>
2025-03-22 17:17:54 -0300

    hero sizes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,39 +1,86 @@
 import styled from "styled-components";
 import monkImg from "../../assets/monks-hero-image.svg";
+import scrollImg from "../../assets/scroll.svg";
 
 export const SSection = styled.section`
   background-color: var(--color-bg-dark);
-  // background: var(--color-bg-dark) url('public/monks-hero-image.svg') right center no-repeat;
-  background-size: cover;
-  color:#EAE8E4;
-  padding: 0 3rem;
+  border-radius: 0 0 30px 30px;
+  overflow: hidden;
+  position:relative;
+  display:flex;
+  align-items: stretch;
+  min-height:100dvh;
   padding-top: var(--header-height);
-  height: calc(100dvh - var(--header-height));
+  box-sizing: border-box;
+  @media (max-width: 768px) {
+    min-height:auto;
+    height: 100dvh;
+    border-radius:0;
+  }
+`;
+
+export const SDivCopy = styled.div`
+  max-width:50%;
+  box-sizing: border-box;
+  padding: 0 0 0 3rem;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  color:#EAE8E4;
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 1.2rem;
+    font-weight: 200;
+    line-height: 2;
+  }
+  img {
+    margin:2rem auto;
+  }
+  @media (max-width: 768px) {
+    max-width:70%;
+    padding: 0 0 0 1rem;
+    h1 {
+      font-size: 2rem;
+    }
+    p {
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const SDivCopyContent = styled.div`
+  flex-grow:1;
+  flex-shrink:0;
+  display:flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
-  gap: 1rem;
-  position:relative;
-  overflow:hidden;
-  border-radius: 0 0 30px 30px;
 `;
 
 export const SImage = styled.img`
   position: absolute;
-  right: 0;
-  transform: translateX(20%);
-  top: 0;
   height: 100%;
+  width: auto;
+  right: 0;
+  top:0;
+  @media (max-width: 768px) {
+    left:40%;
+  }
 `;
+
 
 export default function Hero() {
   return (
     <SSection id="hero">
-      <div>
-        <h1>Lorem ipsum dolor sit amet consectetur</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero ea animi laborum voluptate fugit ab rerum ad quae, eum, molestiae quas asperiores veritatis iure, sequi tenetur minima dolore repellat ipsum!</p>
-      </div>
+      <SDivCopy>
+        <SDivCopyContent>
+          <h1>Lorem ipsum dolor sit amet consectetur</h1>
+          <p>Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra</p>
+        </SDivCopyContent>
+        <img src={scrollImg} alt="Scroll" width="80px" />
+      </SDivCopy>
       <SImage src={monkImg} alt="Imagem" />
     </SSection>
   )
