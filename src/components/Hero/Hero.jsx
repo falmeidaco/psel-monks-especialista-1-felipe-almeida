b79e7f542c4b9b79bec74b2a549c6771be45e15d
falmeidaco <felipealmeidacosta@gmail.com>
2025-03-23 20:23:35 -0300

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,86 +1,109 @@
 import styled from "styled-components";
 import monkImg from "../../assets/monks-hero-image.svg";
-import scrollImg from "../../assets/scroll.svg";
+import Sprites from "../misc/Sprites";
 
-export const SSection = styled.section`
+export const HeroStyled = styled.div`
   background-color: var(--color-dark);
+  min-height:600px;
   border-radius: 0 0 30px 30px;
-  overflow: hidden;
-  position:relative;
   display:flex;
   align-items: stretch;
-  min-height:600px;
-  padding-top: var(--header-height);
-  margin-bottom:3rem;
+  padding-right: 0rem;
+
+  .text {
+    align-content: center;
+    color:#EAE8E4;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top:5rem;
+    padding-bottom:2rem;
+    .copy {
+      flex-grow: 1;
+      align-content: center;
+      h1 {
+        font-size: 3rem;
+        margin-bottom: 1rem;
+      }
+      p {
+        font-size: 1.5rem;
+        line-height: 1.5;
+      }
+    }
+    .scroll {
+      text-align: center;
+      svg {
+        width: 100px;
+      }
+    }
+  }
+
+  .media {
+    border-bottom-right-radius: 30px;
+    overflow: hidden;
+    position: relative;
+    flex-basis: 500px;
+    flex-shrink: 0;
+    z-index: 0;
+    img {
+      left: 0;
+      position: absolute;
+      height: 100%;
+    }
+  }
+
   @media (max-width: 768px) {
-    min-height:auto;
-    height: 100dvh;
+    min-height:unset;
     border-radius:0;
-  }
-`;
-
-export const SDivCopy = styled.div`
-  max-width:50%;
-  padding: 0 0 0 3rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  color:#EAE8E4;
-  h1 {
-    font-size: 3rem;
-    margin-bottom: 1rem;
-  }
-  p {
-    font-size: 1.2rem;
-    font-weight: 200;
-    line-height: 2;
-  }
-  img {
-    margin:2rem auto;
-  }
-  @media (max-width: 768px) {
-    max-width:70%;
-    padding: 0 0 0 1rem;
-    h1 {
-      font-size: 2rem;
+    position:relative;
+    overflow:hidden;
+    .text {
+      position:relative;
+      z-index:10;
+      min-height: 500px;
+      max-width: 50dvw;
+      align-items: center;
+      .copy {
+        h1 {
+          font-size: 1.5rem;
+        }
+        p {
+          font-size: 1rem;
+        }
+      }
+      .scroll {
+        svg {
+          width:60px;
+          height:60px;
+        }
+      }
     }
-    p {
-      font-size: 1rem;
+    .media {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      top:0;
+      right:-40%;
+      flex-basis: 100px;
     }
   }
 `;
-
-export const SDivCopyContent = styled.div`
-  flex-grow:1;
-  flex-shrink:0;
-  display:flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-export const SImage = styled.img`
-  position: absolute;
-  height: 100%;
-  width: auto;
-  right: -10%;
-  top:0;
-  @media (max-width: 768px) {
-    left:40%;
-  }
-`;
-
 
 export default function Hero() {
   return (
-    <SSection id="hero" className="max-width">
-      <SDivCopy>
-        <SDivCopyContent>
+    <HeroStyled as="section" className="max-width">
+      <div className="text">
+        <div className="copy">
           <h1>Lorem ipsum dolor sit amet consectetur</h1>
           <p>Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra</p>
-        </SDivCopyContent>
-        <img src={scrollImg} alt="Scroll" width="80px" />
-      </SDivCopy>
-      <SImage src={monkImg} alt="Imagem" />
-    </SSection>
+        </div>
+        <div class="scroll">
+          <Sprites id="svg-scroll" />
+        </div>
+      </div>
+      <div className="media">
+        <img src={monkImg} alt="Imagem" />
+      </div>
+    </HeroStyled>
   )
 }
